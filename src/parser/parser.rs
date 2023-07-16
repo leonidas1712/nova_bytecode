66ceb4b08694633f5d39d3897ca5ac79122faa95
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-07-16 21:43:47 +0800

    Add test for binary ops

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -358,9 +358,20 @@ fn test_parse() {
 }
 
 #[test]
-fn test_parse2() {
+fn test_parse_ops() {
     let mut p=Parser::new("-");
     let mut chunk=Chunk::new();
+
+    // let v=vec![
+        // 1 + 2 - 3 * 5 + 6 / 9 = -12
+        // 3 * 4 + 7 - 8 / 2 = 15
+        // 10 - 2 * 4 + 6 / 3 = 4
+        // 8 / 2 - 1 + 5 * 2 = 13
+        // 6 + 9 / 3 - 4 * 2 = 1
+
+        // 2 * 5 - 3 + 8 / 4 = 9
+        // 5 + 3 / 6 - 2 * 4 = -3
+    // ];
 
 
     let res=p.compile(&mut chunk);
